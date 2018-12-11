355f1045a3b5fb553a6706709034fa0a5305a948
Aaron11041 <amartinezsoria@alumnat.iessacolomina.es>
2018-12-11 20:12:00 +0100

    El ejercicio del moodle

---
+++ webhtml5/js/java.js
@@ -0,0 +1,9 @@
+var precio=0;
+var total=0;
+var concepto=0;
+var cantidad=0;
+
+function myFunction() {
+  //document.getElementById("ticket").innerHTML = document.getElementById("ticket").innerHTML = "";
+  document.getElementById("ticket").innerHTML ="Precio:" + cantidad;
+}
